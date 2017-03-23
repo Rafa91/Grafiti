e604e98ad6f56fe87805f29731340ce0e989f283
Rafael Navarro <rafaelnavarro@MacBook-Pro-de-Rafael.local>
2017-03-23 13:37:41 +0100

    Initial Commit

---
+++ Graffiti/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Graffiti
+//
+//  Created by Rafael Navarro on 23/3/17.
+//  Copyright © 2017 Rafael Navarro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
